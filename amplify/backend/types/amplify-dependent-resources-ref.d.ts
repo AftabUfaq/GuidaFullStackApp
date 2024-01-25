dbcffb0660570d8743390cf57774520119837da3
Yanyu Zhang <yyzhangtj@126.com>
2024-01-24 22:26:59 -0800

    Update Amplify file

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "figmaguida1bff17b5CreateAuthChallenge": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "figmaguida1bff17b5DefineAuthChallenge": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- index.js
+++
@@ -1,4 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './src/App'; // or the correct path to your main app component
-
-registerRootComponent(App);
